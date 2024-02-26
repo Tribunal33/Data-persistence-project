b864d4983c1d6e6fccbb478178798a52988458e0
Tribunal33 <Tribunal33@users.noreply.github.com>
2024-02-25 23:34:03 -0800

    Update PersistenceManager.cs
    Adding basic save/load functionality

--- Assets/Scripts/PersistenceManager.cs
+++ Assets/Scripts/PersistenceManager.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PersistenceManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public PersistenceManager Instance;
+    public static string playerName;
+    public static int playerHighScore;
+    public int playerScore;
+    [Serializable]
+    public class SavePlayerData{
+        public string playerName;
+        public int playerScore;
+
+    }
+    void Awake(){
+        if(Instance != null){
+            Destroy(Instance);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadHighScore();
+    }
+    public void SavePlayerInfo(){
+        SavePlayerData data = new SavePlayerData();
+        data.playerName = playerName;
+        data.playerScore = playerScore;
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/saveplayerdata.json", json);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void LoadHighScore(){
+        string path = Application.persistentDataPath + "/saveplayerdata.json";
+
+        if(File.Exists(path)){
+            string json = File.ReadAllText(path);
+            SavePlayerData data = JsonUtility.FromJson<SavePlayerData>(json);
+
+            playerName = data.playerName;
+            playerScore = data.playerScore;
+        }
+    }
+
+    public void StartGame(){
+        SceneManager.LoadScene("main");
     }
 }
